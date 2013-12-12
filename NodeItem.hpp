c9f6e9730039d30f980d8de39dc146afe3bb0a8f
Mika Attila <radiantstatue@gmail.com>
2013-12-12 03:16:34 +0100

    Move NodeItem implementation from NodeItem.hpp to NodeItem.cpp

--- NodeItem.hpp
+++ NodeItem.hpp
@@ -10,55 +10,9 @@
 
 struct NodeItem : public QTreeWidgetItem
 {
-    NodeItem(const nl::node& n) :
-        node(n)
-    {
-        setText(0, QString::fromStdString(node.name()));
-        setText(1, typeStr());
-        setText(2, valueStr());
-    }
-
-    QString typeStr()
-    {
-        switch (node.data_type())
-        {
-        case nl::node::type::audio:
-            return "audio";
-        case nl::node::type::bitmap:
-            return "bitmap";
-        case nl::node::type::real:
-            return "real";
-        case nl::node::type::integer:
-            return "integer";
-        case nl::node::type::string:
-            return "string";
-        case nl::node::type::vector:
-            return "vector";
-        case nl::node::type::none:
-            return QString();
-        }
-
-        return QString("?????");
-    }
-
-    QString valueStr()
-    {
-        switch (node.data_type())
-        {
-        case nl::node::type::real:
-        case nl::node::type::integer:
-        case nl::node::type::string:
-            return QString::fromStdString(node.get_string());
-        case nl::node::type::vector: {
-            const auto vec = node.get_vector();
-            return QString("{%1, %2}").arg(vec.first).arg(vec.second);
-        }
-        default:
-            return QString();
-        }
-
-        return QString();
-    }
+    NodeItem(const nl::node& n);
+    QString typeStr();
+    QString valueStr();
 
     nl::node node;
     bool childrenAdded = false;
