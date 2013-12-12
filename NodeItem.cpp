96a7760c5061a601d3c656147b7f1ce8fc45ffda
Mika Attila <radiantstatue@gmail.com>
2013-12-12 03:24:34 +0100

    Refactor NodeItem

--- NodeItem.cpp
+++ NodeItem.cpp
@@ -1,51 +1,53 @@
 #include "NodeItem.hpp"
 
+namespace {
+    QString nodeTypeAsString(nl::node node)
+    {
+        switch (node.data_type())
+        {
+        case nl::node::type::audio:
+            return "audio";
+        case nl::node::type::bitmap:
+            return "bitmap";
+        case nl::node::type::real:
+            return "real";
+        case nl::node::type::integer:
+            return "integer";
+        case nl::node::type::string:
+            return "string";
+        case nl::node::type::vector:
+            return "vector";
+        case nl::node::type::none:
+            return QString();
+        }
+    
+        return QString("?????");
+    }
+    
+    QString nodeValueAsString(nl::node node)
+    {
+        switch (node.data_type())
+        {
+        case nl::node::type::real:
+        case nl::node::type::integer:
+        case nl::node::type::string:
+            return QString::fromStdString(node.get_string());
+        case nl::node::type::vector: {
+            const auto vec = node.get_vector();
+            return QString("{%1, %2}").arg(vec.first).arg(vec.second);
+        }
+        default:
+            return QString();
+        }
+    
+        return QString();
+    }
+}
+
 NodeItem::NodeItem(const nl::node& n) :
     node(n)
 {
     setText(0, QString::fromStdString(node.name()));
-    setText(1, typeStr());
-    setText(2, valueStr());
-}
-
-QString NodeItem::typeStr()
-{
-    switch (node.data_type())
-    {
-    case nl::node::type::audio:
-        return "audio";
-    case nl::node::type::bitmap:
-        return "bitmap";
-    case nl::node::type::real:
-        return "real";
-    case nl::node::type::integer:
-        return "integer";
-    case nl::node::type::string:
-        return "string";
-    case nl::node::type::vector:
-        return "vector";
-    case nl::node::type::none:
-        return QString();
-    }
-
-    return QString("?????");
-}
-
-QString NodeItem::valueStr()
-{
-    switch (node.data_type())
-    {
-    case nl::node::type::real:
-    case nl::node::type::integer:
-    case nl::node::type::string:
-        return QString::fromStdString(node.get_string());
-    case nl::node::type::vector: {
-        const auto vec = node.get_vector();
-        return QString("{%1, %2}").arg(vec.first).arg(vec.second);
-    }
-    default:
-        return QString();
-    }
-
-    return QString();
+    setText(1, nodeTypeAsString(node));
+    setText(2, nodeValueAsString(node));
 }
--- NodeItem.hpp
+++ NodeItem.hpp
@@ -4,16 +4,12 @@
 #include <QTreeWidgetItem>
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wshadow"
-#include "NoLifeNx/nx.hpp"
 #include "NoLifeNx/node.hpp"
 #pragma GCC diagnostic pop
 
 struct NodeItem : public QTreeWidgetItem
 {
     NodeItem(const nl::node& n);
-    QString typeStr();
-    QString valueStr();
-
     nl::node node;
     bool childrenAdded = false;
 };
